76fdc546ac58d59192fb528363c22eb9f4e4c610
leezeeyee <easilylazy@gmail.com>
2021-06-09 17:12:49 +0800

    remove useless

--- 15Puzzle/main.cpp
+++ 15Puzzle/main.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 
 int main(void){
-    Board demo(3);
+    Board demo(4);
     demo.print();
-    demo.shuffle(38);
+    demo.shuffle(29);
     // demo.possible_direction();
     // demo.exec_direction(Up);
     // demo.print();
@@ -20,33 +20,4 @@ int main(void){
     // demo.display();
     return 0;
 
-
-
-    vector<int> test;//(9);
-    vector<int> test2;
-    auto foo1=demo.board;
-    auto foo2=demo.board;
-    auto foo3=demo.board;
-    foo3[0][0]=3;
-    if(foo1==foo2){
-        cout<<"fe1"<<endl;
-    }
-     if(foo1==foo3){
-        cout<<"fe2"<<endl;
-    }
-    for(auto row:demo.board){
-        copy(row.begin(),row.end(),back_inserter(test));
-    }
-    for(auto t:test){
-        cout<<t<<endl;
-    }
-    if(test==test2){
-        cout<<"e1"<<endl;
-    }
-    for(auto row:demo.board){
-        copy(row.begin(),row.end(),back_inserter(test2));
-    }
-    if(test==test2){
-        cout<<"e2"<<endl;
-    }
 }
