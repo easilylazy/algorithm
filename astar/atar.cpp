21917a1e53c15cd7909fdb0f9ba2b60570035488
leezeeyee <easilylazy@qq.com>
2021-06-14 18:59:30 +0800

    add overload for ==

--- astar/atar.cpp
+++ astar/atar.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
+#include <random>
 #include "atar.h"
 
 MapGraph::MapGraph(int size){
+    astar();
+    map_size=size;
     graph=vector<vector<int>>(size);
     for(int i=0;i<size;i++){
         graph[i]=vector<int>(size);
         for(int j=0;j<size;j++){
+            graph[i][j]=rand()%(cost_max-cost_min)+cost_min;
             cout<<graph[i][j]<<" ";
         }
         cout<<endl;
+    }
+    src=Site(0,0);
+    dst=Site(size-1,size-1);
+    astar();
+}
+void MapGraph::astar(){
+    if(src==dst){
+        cout<<"equal"<<endl;
+    }else{
+        cout<<"unequal"<<endl;
+
     }
 }
--- astar/atar.h
+++ astar/atar.h
@@ -1,8 +1,34 @@
 #include <vector>
 using namespace std;
+class Site{
+private:
+    int x;
+    int y;
+public:
+    Site(){
+        x=0;
+        y=0;
+    }
+    Site(int row,int col){
+        x=row;
+        y=col;
+    }
+    bool operator==(const Site &site){
+        if(this->x==site.x&&this->y==site.y){
+            return true;
+        }
+        return false;
+    }
+};
 class MapGraph{
 private:
+    int cost_min=0;
+    int cost_max=10;
+    int map_size;
     vector<vector<int>> graph;
+    Site src,dst;
+
 public:
     MapGraph(int size);
+    void astar();
 };
